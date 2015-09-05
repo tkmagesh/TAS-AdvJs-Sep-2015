8f7d6cf75f9078ffa638cce278f71305c4b74da2
tkmagesh <tkmagesh77@gmail.com>
2015-09-05 15:35:31 +0530

    primeFinder problem statement

---
+++ Day-01/02-Closures/primeFinder.js
@@ -0,0 +1,11 @@
+/*function isPrime(n){
+    console.log("Processing ", n);
+    if (n <= 3) return true;
+    for(var i=2; i<=(n/2); i++)
+        if (n % 2 === 0) return false;
+    return true;
+}*/
+
+//Write a function that checks if the given number is a prime number or not.
+//The function SHOULD NOT recalculate again and again if the same number is given as input
+//If a number is processed once, the function should maintain the resulf of the given number in a cache and return the result from the cache
